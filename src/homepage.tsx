d089a8728e6b25bc4939f981b0eda76a82137770
Paul Grad <paulgrad3rd@gmail.com>
2023-02-21 00:46:21 -0800

    Vanity

--- src/homepage.tsx
+++ src/homepage.tsx
@@ -100,7 +100,7 @@ export default function Homepage() {
                 <section className="intro">
                     <div className="flavor-text">
                         <p>Frontend Web Developer based out of Oakland, CA.</p>
-                        <p>Interested in AI, languages, books, and cats.</p>
+                        <p>Interested in AI, languages, space, and cats.</p>
                     </div>
                     <a className="resume fade-in" href="https://bit.ly/3Z0H3PK">Resumé</a>
                     <div className="socials fade-in">
