4d728d0ce4ca1535c40bf46559c90dfd7fbeea1f
Paul Grad <paulgrad3rd@gmail.com>
2024-03-16 13:13:05 -0700

    Hide the iframe from screen readers

--- src/Host.tsx
+++ src/Host.tsx
@@ -2,6 +2,11 @@ import Iframe from "react-iframe";
 import { useParams } from "react-router-dom";
 import "./Host.css";
 
+// Iframes can't be explored by screen
+// readers, so it's best to hide them.
+
+// If a disabled user wants to access
+// the project, they can click on the link.
 export default function Host() {
   const { project } = useParams();
   if (project) {
@@ -12,7 +17,13 @@ export default function Host() {
           Live at
           <a href={url}>{url}</a>
         </h3>
-        <Iframe className="host" url={url} width="100%" height="100%" />
+        <Iframe
+          ariaHidden
+          className="host"
+          url={url}
+          width="100%"
+          height="100%"
+        />
       </div>
     );
   }
