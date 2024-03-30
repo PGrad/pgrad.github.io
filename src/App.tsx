3b11a9341fbeb6593d3de256d4ff8096c5012efc
Paul Grad <paulgrad3rd@gmail.com>
2024-03-29 19:02:11 -0700

    Add react-scroll
    I use the react-scroll library so users
    can smoothly scroll between the different
    routes on the page, and the navbar links
    highlight according to where you are.

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
-import { HashRouter, Link, Route, Routes } from "react-router-dom";
 import { createTheme, Theme, useMediaQuery } from "@mui/material";
+import { Element, Link } from 'react-scroll';
 import Homepage from "./homepage";
 import Host from "./Host";
 import "./App.css";
@@ -133,14 +133,14 @@ function App() {
     [prefersDarkMode]
   );
   const [open, setOpen] = useState(false);
-  const [selectedProject, setSelectedProject] = useState(() => {
-    const initVal = localStorage.getItem("selected") || "-1";
-    return Number(initVal);
+  const [atHome, setAtHome] = useState(() => {
+    const initVal = localStorage.getItem("at_home") || "true";
+    return Boolean(initVal);
   });
 
   useEffect(() => {
-    localStorage.setItem("selected", String(selectedProject));
-  }, [selectedProject]);
+    localStorage.setItem("at_home", String(atHome));
+  }, [atHome]);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -160,8 +160,8 @@ function App() {
   const lightBkgd: string = radialString([1, 79, 82, 0.5], [0, 221, 225, 0.2]);
   const darkBkgd: string = radialString([255, 113, 0, 1], [0, 0, 0, 1]);
 
-  const setSelectedFactory = (idx: number) => () => {
-    setSelectedProject(idx);
+  const setSelected = (to: string) => {
+    setAtHome(to === 'home');
   };
 
   const onlyProjects = () => {
@@ -170,139 +170,145 @@ function App() {
     return projects;
   };
 
-  const isSelected = (idx: number) => selectedProject === idx;
-
   return (
     <ThemeProvider theme={theme}>
-      <HashRouter basename="/">
-        <Box sx={{ display: "flex" }}>
-          <CssBaseline />
-          <AppBar position="fixed" open={open} sx={{marginBottom: "5em" }}>
-            <Toolbar
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open} sx={{marginBottom: "5em" }}>
+          <Toolbar
+            sx={{
+              backgroundColor: prefersDarkMode ? "black" : "white",
+              width: "100vw",
+            }}
+          >
+            <Typography
+              variant="h6"
+              noWrap
               sx={{
-                backgroundColor: prefersDarkMode ? "black" : "white",
-                width: "100vw",
+                flexGrow: 1,
+                display: "flex",
+                justifyContent: "space-between",
               }}
+              component="div"
             >
-              <Typography
-                variant="h6"
-                noWrap
-                sx={{
-                  flexGrow: 1,
-                  display: "flex",
-                  justifyContent: "space-between",
-                }}
-                component="div"
+              <Link
+                spy
+                smooth
+                className="bare-link title"
+                to="home"
+                onSetActive={setSelected}
               >
-                <Link
-                  onClick={setSelectedFactory(-1)}
-                  className="bare-link title"
-                  to="/"
-                >
-                  {selectedProject === -1 ? "Paul's Projects" : "Home"}
-                </Link>
-                <div className="project-links">
-                  {isDesktop
-                    ? onlyProjects().map((project, idx) => (
-                        <Link
-                          key={idx}
-                          onClick={setSelectedFactory(idx)}
-                          className={`bare-link
-                        link
-                        ${isSelected(idx) ? "sel-link" : "not-sel-link"}`}
-                          to={`${project.link}/`}
-                        >
-                          {project.name}
-                        </Link>
-                      ))
-                    : ""}
-                </div>
-              </Typography>
-              {!isDesktop ? (
-                <IconButton
-                  aria-label="open drawer"
-                  edge="end"
-                  onClick={handleDrawerOpen}
-                  sx={{ ...(open && { display: "none" }) }}
-                >
-                  <MenuIcon />
-                </IconButton>
-              ) : (
-                ""
-              )}
-            </Toolbar>
-          </AppBar>
-          <Main
-            open={open}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              background: prefersDarkMode ? darkBkgd : lightBkgd,
-              // Use the styling from the styled component.
-              margin: isDesktop ? 0 : "",
-              overflowY: "auto",
-              overflowX: "hidden",
-            }}
-          >
-                <Homepage />
-                <Work />
-              {onlyProjects().slice(1).map(project => (
-                  <Host project={project.link} />
-              ))}
-          </Main>
-          <Drawer
-            sx={{
-              display: isDesktop ? "none" : "flex",
-              width: drawerWidth,
-              flexShrink: 0,
-              "& .MuiDrawer-paper": {
-                width: drawerWidth,
-              },
-            }}
-            anchor="right"
-            variant="persistent"
-            open={open}
-          >
-            <DrawerHeader>
-              <IconButton onClick={handleDrawerClose}>
-                {theme.direction === "rtl" ? (
-                  <ChevronLeftIcon />
-                ) : (
-                  <ChevronRightIcon />
-                )}
-              </IconButton>
-            </DrawerHeader>
-            <Divider />
-            {categories.map((category, idx) => (
-              <div key={idx} className="category">
-                <h3 className="category-title title">{category.name}</h3>
-                <Divider />
-                <List disablePadding dense>
-                  {category.projects.map((project) => (
-                    <ListItem
-                      key={project.idx}
-                      onClick={setSelectedFactory(project.idx)}
-                      className="item"
-                      disablePadding
-                    >
+                {!atHome ? "Paul's Projects" : "Home"}
+              </Link>
+              <div className="project-links">
+                {isDesktop
+                  ? onlyProjects().map((project, idx) => (
                       <Link
-                        className={`bare-link link ${
-                          isSelected(project.idx) ? "sel-link" : "not-sel-link"
-                        }`}
-                        to={`${project.link}/`}
+                        key={idx}
+                        onSetActive={setSelected}
+                        spy
+                        smooth
+                        activeClass="sel-link"
+                        className="bare-link link"
+                        to={project.link}
                       >
                         {project.name}
                       </Link>
-                    </ListItem>
-                  ))}
-                </List>
-                <Divider />
+                    ))
+                  : ""}
               </div>
+            </Typography>
+            {!isDesktop ? (
+              <IconButton
+                aria-label="open drawer"
+                edge="end"
+                onClick={handleDrawerOpen}
+                sx={{ ...(open && { display: "none" }) }}
+              >
+                <MenuIcon />
+              </IconButton>
+            ) : (
+              ""
+            )}
+          </Toolbar>
+        </AppBar>
+        <Main
+          open={open}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            background: prefersDarkMode ? darkBkgd : lightBkgd,
+            // Use the styling from the styled component.
+            margin: isDesktop ? 0 : "",
+            overflowY: "auto",
+            overflowX: "hidden",
+          }}
+        >
+            <Element name="home">
+              <Homepage />
+            </Element>
+            <Element name="work">
+              <Work />
+            </Element>
+            {onlyProjects().slice(1).map(project => (
+              <Element key={project.idx} name={project.link}>
+                <Host project={project.link} />
+              </Element>
             ))}
-          </Drawer>
-        </Box>
-      </HashRouter>
+        </Main>
+        <Drawer
+          sx={{
+            display: isDesktop ? "none" : "flex",
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+            },
+          }}
+          anchor="right"
+          variant="persistent"
+          open={open}
+        >
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          {categories.map((category, idx) => (
+            <div key={idx} className="category">
+              <h3 className="category-title title">{category.name}</h3>
+              <Divider />
+              <List disablePadding dense>
+                {category.projects.map((project) => (
+                  <ListItem
+                    key={project.idx}
+                    className="item"
+                    disablePadding
+                  >
+                    <Link
+                      className="bare-link link"
+                      onSetActive={setSelected}
+                      spy
+                      smooth
+                      activeClass="sel-link"
+                      to={project.link}
+                    >
+                      {project.name}
+                    </Link>
+                  </ListItem>
+                ))}
+              </List>
+              <Divider />
+            </div>
+          ))}
+        </Drawer>
+      </Box>
     </ThemeProvider>
   );
 }
