0c4d911c596e484b447466b590e288b3e0628ad0
Paul Grad <paulgrad3rd@gmail.com>
2025-03-07 13:57:27 -0800

    Show static image for Hidden Gems project
    spotify doesn't work well with iframes, and an
    error page is shown when trying to sign in.
    
    Instead, just show an image.

--- src/Host.tsx
+++ src/Host.tsx
@@ -15,18 +15,22 @@ export default function Host({ project }: HostParams) {
   if (project) {
     const url = project.external ? project.link : `https://pgrad.github.io/${project.link}`;
     return (
-      <div className="host-container">
+      <div className="host-container" style={project.name === 'Hidden Gems' ? {} : { height: '100vh' }}>
         <h3 className="project-link">
           Live at&nbsp;
           <a href={url}>{url}</a>
         </h3>
-        <Iframe
-          ariaHidden
-          className="host"
-          url={url}
-          width="100%"
-          height="100%"
-        />
+        {/* Spotify doesn't play nice with iframes, so don't bother. */}
+        {project.name === 'Hidden Gems' ?
+          <img src="thundercat.png" style={{ width: 'fit-content', maxWidth: '100%' }} alt="Hidden Gems" /> :
+          <Iframe
+            ariaHidden
+            className="host"
+            url={url}
+            width="100%"
+            height="100%"
+          />
+        }
       </div>
     );
   }
