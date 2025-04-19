086e5a285fd81e20d0ac4f36192931879f8c001e
PGrad <paulgrad3rd@gmail.com>
2025-04-19 16:21:32 -0700

    Leave the PWA out for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import AstroPWA from '@vite-pwa/astro'
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import node from '@astrojs/node';
@@ -6,7 +5,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pgrad.github.io',
-  integrations: [react(), AstroPWA()],
+  integrations: [react()],
   adapter: node({
     mode: 'standalone'
   })
