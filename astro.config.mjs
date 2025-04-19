6f86d54e229bbbc48d1aea6b8791633851cb173a
PGrad <paulgrad3rd@gmail.com>
2025-04-19 16:13:20 -0700

    Use astro Github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import AstroPWA from '@vite-pwa/astro'
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pgrad.github.io',
   integrations: [react(), AstroPWA()],
-
   adapter: node({
     mode: 'standalone'
   })
