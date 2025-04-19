4a02ce0646f5479b5e2b693e7c86e95fd6dca15a
PGrad <paulgrad3rd@gmail.com>
2025-04-19 16:35:13 -0700

    Deploy build client folder, turn PWA back on

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import AstroPWA from '@vite-pwa/astro'
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import node from '@astrojs/node';
@@ -5,8 +6,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pgrad.github.io',
-  base: '/client',
-  integrations: [react()],
+  integrations: [react(), AstroPWA()],
   adapter: node({
     mode: 'standalone'
   })
