650a91b80a20668c05e5c338aad53d57a0b59a3e
PGrad <paulgrad3rd@gmail.com>
2025-04-22 11:45:53 -0700

    Let Vite PWA + Workbox do all the work for me
    Vite PWA generates a service worker on its own;
    there is no need to write one nor include it.
    
    It also generates the manifest, which I describe
    in the AstroPWA config in the astro.config.mjs file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,49 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://paulgrad.netlify.app/',
-  integrations: [react(), AstroPWA()],
+  integrations: [react(),
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      registerType: 'autoUpdate',
+      manifest: {
+        "background_color": "#ffffff",
+        "description": "The personal website of Paul Grad, a Frontend Developer based in Oakland, CA.",
+        "dir": "ltr",
+        "display": "standalone",
+        "name": "Paul's Projects",
+        "orientation": "any",
+        "scope": "https://paulgrad.netlify.app/",
+        "short_name": "All Paul",
+        "start_url": "/",
+        "theme_color": "#000000",
+        "icons": [
+          {
+            "src": "/pwa_icon.png",
+            "sizes": "600x600"
+          }
+        ],
+        "id": "?source=paul_portfolio",
+        "lang": "en"
+      },
+      pwaAssets: {
+        config: true,
+      },
+      workbox: {
+        navigateFallback: '/',
+        maximumFileSizeToCacheInBytes: 3000000,
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/$/],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    })
+  ],
   adapter: netlify({
     edgeMiddleware: true,
   }),
