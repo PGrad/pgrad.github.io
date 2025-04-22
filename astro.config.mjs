db57c90e96561ee60be6876e6102bfbf6b69ef74
PGrad <paulgrad3rd@gmail.com>
2025-04-22 10:23:37 -0700

    Deploy thru Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pgrad.github.io',
   integrations: [react()],
-  adapter: node({
-    mode: 'browser'
-  })
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
 });
