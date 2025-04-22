7b4798afc8ae153f41541710284a7d1742eb7123
PGrad <paulgrad3rd@gmail.com>
2025-04-22 10:38:55 -0700

    Revert "Turn off PWA"
    This reverts commit a03b4d38bd8827c09a6a8f3bc252775b8c0a2255.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
+import AstroPWA from '@vite-pwa/astro'
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pgrad.github.io',
-  integrations: [react()],
+  integrations: [react(), AstroPWA()],
   adapter: netlify({
     edgeMiddleware: true,
   }),
