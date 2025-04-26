a792f587e500ee948fade6992a71b8606840662e
PGrad <paulgrad3rd@gmail.com>
2025-04-25 21:58:19 -0700

    Add log for netlify endpoint

--- src/pages/api/webhook.json.ts
+++ src/pages/api/webhook.json.ts
@@ -4,8 +4,10 @@ import { purgeCache } from "@netlify/functions";
 export async function POST({ request }) {
   const body = await request.json();
 
+  console.log("Received webhook from Contentful", body);
   // https://developers.netlify.com/guides/how-to-do-advanced-caching-and-isr-with-astro/#how-to-revalidate-your-pages-on-demand-with-cache-tags-and-webhooks
   if (request.headers.get("X-Contentful-Webhook-Secret") !== import.meta.env.CONTENTFUL_WEBHOOK_SECRET) {
+    console.log("Unauthorized request");
     return new Response(JSON.stringify({
         message: "Unauthorized"
     }), {
@@ -16,6 +18,7 @@ export async function POST({ request }) {
     });
   }
 
+  console.log("Good request");
   // Refresh specific pages on publish, but all refresh
   // the entire collection on delete, new items etc.
   await purgeCache({ tags: [body.sys.id, "markdownPage"] });
