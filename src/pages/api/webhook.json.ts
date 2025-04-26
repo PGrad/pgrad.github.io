1f30515d3c4361144ce7472de0285f9c96beb01e
PGrad <paulgrad3rd@gmail.com>
2025-04-25 21:32:10 -0700

    Apparently the API route isn't being created
    Empty response

--- src/pages/api/webhook.json.ts
+++ src/pages/api/webhook.json.ts
@@ -5,12 +5,16 @@ export async function POST({ request }) {
 
   // https://developers.netlify.com/guides/how-to-do-advanced-caching-and-isr-with-astro/#how-to-revalidate-your-pages-on-demand-with-cache-tags-and-webhooks
   if (request.headers.get("X-Contentful-Webhook-Secret") !== import.meta.env.CONTENTFUL_WEBHOOK_SECRET) {
-    return new Response("Unauthorized", { status: 401 });
+    return new Response(JSON.stringify({
+        message: "Unauthorized"
+    }), { status: 401 });
   }
 
   // Refresh specific pages on publish, but all refresh
   // the entire collection on delete, new items etc.
   await purgeCache({ tags: [body.sys.id, "markdownPage"] });
 
-  return new Response(`Revalidated entry with id ${body.sys.id}`, { status: 200 });
+  return new Response(JSON.stringify({
+    message: `Revalidated entry with id ${body.sys.id}`,
+  }), { status: 200 });
 }
