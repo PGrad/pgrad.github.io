d3d4b2407edbcf17de95dbf538decba070f38823
PGrad <paulgrad3rd@gmail.com>
2025-04-25 20:23:27 -0700

    Use a webhook to refresh blog posts on publish
    It's really amazing that the whole website
    can be fetched once and then cached in a PWA.
    
    However if the content changes, the blog posts
    still need to update. Netlify's docs recommend
    using a webhook.

---
+++ src/pages/api/webhook.json.ts
@@ -0,0 +1,16 @@
+import { purgeCache } from "@netlify/functions";
+
+export async function POST({ request }) {
+  const body = await request.json();
+
+  // https://developers.netlify.com/guides/how-to-do-advanced-caching-and-isr-with-astro/#how-to-revalidate-your-pages-on-demand-with-cache-tags-and-webhooks
+  if (request.headers.get("X-Contentful-Webhook-Secret") !== import.meta.env.CONTENTFUL_WEBHOOK_SECRET) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  // Refresh specific pages on publish, but all refresh
+  // the entire collection on delete, new items etc.
+  await purgeCache({ tags: [body.sys.id, "markdownPage"] });
+
+  return new Response(`Revalidated entry with id ${body.sys.id}`, { status: 200 });
+}
